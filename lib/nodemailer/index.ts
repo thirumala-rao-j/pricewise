0c0398661114497ab1efdacf687b4a33412406e9
thirumalaraojuvvisetti <thirumala.juvvisetti@gmail.com>
2023-10-06 19:31:46 +0530

    Fix email errors

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -95,6 +95,19 @@ export const sendEmail = async (
   emailContent: EmailContent,
   sendTo: string[]
 ) => {
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error, success) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log("Server is ready to take our messages");
+        resolve(success);
+      }
+    });
+  });
+
   const mailOptions = {
     from: "thirumala.juvvisetti@outlook.com",
     to: sendTo,
@@ -102,9 +115,22 @@ export const sendEmail = async (
     subject: emailContent.subject,
   };
 
-  transporter.sendMail(mailOptions, (error: any, info: any) => {
-    if (error) return console.log(error);
-
-    console.log("Email sent: ", info);
+  await new Promise((resolve, reject) => {
+    // send mail
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      } else {
+        console.log(info);
+        resolve(info);
+      }
+    });
   });
+
+  // transporter.sendMail(mailOptions, (error: any, info: any) => {
+  //   if (error) return console.log(error);
+
+  //   console.log("Email sent: ", info);
+  // });
 };
